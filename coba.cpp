f23d307a616cb6e02f8006c472b5bfef68ef9aa6
Hani-hue <isnahabibah8729@gmail.com>
2025-03-20 10:55:46 +0700

    uwaw

--- coba.cpp
+++ coba.cpp
@@ -9,3 +9,16 @@ void input()
     cout << "Memasukkan Jumlah Data pada Array : "; // Membuat Inputan jumlah element Array
     cin >> n;                                       // Memanggil variable inputan n
 }
+
+if (n <= 20)
+{ // membuat kondisi n tidak lebih dari 20
+    break;
+}
+else
+    ;
+{
+    cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; // menampilkan pesan jika data lebih dari 20
+}
+}
+cout << endl;                        // membuat jarak per baris program
+cout << "=================" << endl; // membuat tampilan susunan data element array
