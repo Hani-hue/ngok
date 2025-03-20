7ba3d00455b25107cf6559a822ce964b5ab918ff
Hani-hue <isnahabibah8729@gmail.com>
2025-03-20 10:50:08 +0700

    gatau lek

--- coba.cpp
+++ coba.cpp
@@ -3,3 +3,9 @@ using namespace std;
 
 int arr[20]; // membuat array dengan panjang data 20
 int n;       // membuat variable inputan n
+
+void input()
+{
+    cout << "Memasukkan Jumlah Data pada Array : "; // Membuat Inputan jumlah element Array
+    cin >> n;                                       // Memanggil variable inputan n
+}
