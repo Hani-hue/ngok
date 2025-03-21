991bcec5d28e179884a7e43b4048c4be47c50589
Hani-hue <isnahabibah8729@gmail.com>
2025-03-22 00:32:16 +0700

    ngek

--- coba.cpp
+++ coba.cpp
@@ -45,5 +45,18 @@ void insertionSort()
         j = i - 1;     // step 3
 
         while (j >= 0 && arr[j] > temp) // step 4
+        {
+            arr[j + 1] = arr[j]; // step 4a
+            j--;                 // step 4b
+        }
+        arr[j + 1] = temp; // step 5
     }
+}
+
+vid display()
+{
+    cout << endl; // output baris kosong
+    cout << "===========================" << endl;
+    xout << "Total Passs = " << n - 1 << endl; // output ke layar
+}
 }
