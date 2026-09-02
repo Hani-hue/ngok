b3ab353019ee8b0ae136459f29ed69d57c2360b4
Hani-hue <isnahabibah9827@gmail.com>
2026-09-02 20:11:03 +0700

    Add hello world script with user input and addition functionality

---
+++ hello world.py
@@ -0,0 +1,6 @@
+nama = input("Siapa nama kamu? ")
+print(f"Halo, {nama}! Selamat belajar Git.")
+
+angka1 = int(input("Masukin angka pertama: "))
+angka2 = int(input("Masukin angka kedua: "))
+print(f"Hasil penjumlahan: {angka1 + angka2}")
